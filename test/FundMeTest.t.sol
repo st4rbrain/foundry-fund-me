0ae940706ea1e9de550c80580166e9e111fdcac3
st4rbrain <st4rbrain@example.com>
2023-07-04 17:11:54 +0530

    linked DeployFundMe script with the FundMeTest to avoid multiple contract creation

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "../lib/forge-std/src/Test.sol"; 
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test{
 
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumUSDIsFive() public {
