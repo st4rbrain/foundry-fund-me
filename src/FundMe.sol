6e4bca5e2fa9f6ec91d21ab630906ca72fc33c21
st4rbrain <st4rbrain@example.com>
2023-07-04 14:17:48 +0530

    created a PriceConverter library using the chainlink price feeds

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -6,8 +6,24 @@
 
 pragma solidity ^0.8.18;
 
-contract FundMe {
+import {PriceConverter} from "./PriceConverter.sol";
 
-    
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 constant MINIMUM_USD = 5e18;
+
+    address[] private funders;
+    mapping (address funder => uint256 amountFunded) private addressToAmountFunded;
+
+    function fundMe() external payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Send at least $5");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() external {
+
+    }
 
 }
