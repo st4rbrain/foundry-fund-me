f41bc73724972bab9b9d9f8f54da7e2f35dd95fd
st4rbrain <st4rbrain@example.com>
2023-07-04 18:41:52 +0530

    added mainnetEth priceFeed

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -19,8 +19,9 @@ contract HelperConfig {
     constructor() {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
-        }
-        else {
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
             activeNetworkConfig = getAnvilEthConfig();
         }
     }
@@ -31,6 +32,13 @@ contract HelperConfig {
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         });
         return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns(NetworkConfig memory) {
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return ethConfig;
     }
 
     function getAnvilEthConfig() public pure returns(NetworkConfig memory) {
