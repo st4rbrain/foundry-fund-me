caa264a0836098169f77444721b8a224d50840f2
st4rbrain <cosmofi@wsl.com>
2023-07-05 13:56:48 +0530

    added a remappings.txt file for shorter import paths

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.18;
 
-import {Script} from "../lib/forge-std/src/Script.sol";
+import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script{
